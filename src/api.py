c2c02b76fca89d0bd9ca13200f89f0228d60d3d3
Katyayini-Creates <katyayinis.04@gmail.com>
2026-03-04 22:35:48 +0530

    Frontend Integration: Added HTML UI and connected it with FastAPI backend for password analysis

--- src/api.py
+++ src/api.py
@@ -1,13 +1,40 @@
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# from src.core.checker import analyze_password
+
+# app = FastAPI(title="SecuraWord API")
+
+# class PasswordRequest(BaseModel):
+#     password: str
+
+# @app.post("/analyze")
+# def analyze(request: PasswordRequest):
+#     result = analyze_password(request.password)
+#     return result
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 from src.core.checker import analyze_password
 
 app = FastAPI(title="SecuraWord API")
 
+# Serve frontend folder
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
+
+
 class PasswordRequest(BaseModel):
     password: str
+
 
 @app.post("/analyze")
 def analyze(request: PasswordRequest):
     result = analyze_password(request.password)
     return result
+
+
+@app.get("/")
+def home():
+    return FileResponse("frontend/index.html")
